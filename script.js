4b02eb8b9e655347aff88e7b22d0da4846557118
Simone Cantoni <71756284+cantooo@users.noreply.github.com>
2021-10-28 09:58:42 +0200

    Added progress messages within the button

--- script.js
+++ script.js
@@ -51,7 +51,10 @@ function controllaInput() {
 		time = inputTime.value;
 	}
 
-	if (corretto) CSVFormat(file, new Date(date + "T" + time));
+	if (corretto) {
+		CSVFormat(file, new Date(date + "T" + time));
+		document.querySelector("#button").disabled = true;
+	}
 }
 
 /**
@@ -63,23 +66,29 @@ function controllaInput() {
 function CSVFormat(f, d) {
 	let reader = new FileReader();
 	let data;
+	let button = document.querySelector("#button");
 
 	// Lettura del file
+	button.innerText = "Lettura del file";
 	reader.readAsText(f);
 	reader.addEventListener("load", function (e) {
 		let text = e.target.result;
 
 		// Divisione in righe
+		button.innerText = "Divisione in righe";
 		data = text.split("\r\n");
 
 		// Eliminazione prima e ultima riga
+		button.innerText = "Eliminazione prima e ultima riga";
 		data.splice(0, 1);
 		data.splice(data.length - 1);
 
 		// Divisione in colonne
+		button.innerText = "Divisione in colonne";
 		data[0] = data[0].split(",");
 
 		// Eliminazione colonne non necessarie nella prima riga
+		button.innerText = "Eliminazione colonne non necessarie nella prima riga";
 		data[0].splice(7, 1);
 		data[0].splice(12, 1);
 		data[0].splice(15);
@@ -88,6 +97,7 @@ function CSVFormat(f, d) {
 		data[0].splice(0, 2);
 
 		// Parsing del documento
+		button.innerText = "Parsing del documento";
 		for (let i = 1; i < data.length; i++) {
 			// Divisione in colonne
 			data[i] = data[i].split(",");
@@ -104,6 +114,7 @@ function CSVFormat(f, d) {
 		}
 
 		// Ordinamento per data e ora, intestazione come prima riga
+		button.innerText = "Ordinamento per data e ora";
 		data.sort(function (a, b) {
 			if (typeof a[0] == "string") return -1;
 			if (typeof b[0] == "string") return 1;
@@ -112,6 +123,7 @@ function CSVFormat(f, d) {
 		});
 
 		// Eliminazione righe non necessarie in base alla data fornita
+		button.innerText = "Eliminazione righe non necessarie"
 		for (let i = 1; i < data.length; i++) {
 			if (data[i][0].getTime() <= d.getTime()) {
 				data.splice(i, 1);
@@ -121,6 +133,7 @@ function CSVFormat(f, d) {
 
 		/* Formattazione della data in stringa in un formato compatibile con
 		 * Health CSV Importer */
+		button.innerText = "Formattazione della data";
 		data.forEach((e, i) => {
 			if (i == 0) return;
 			e[0] = "".concat(e[0].getDate(), "/", e[0].getMonth() + 1, "/",
@@ -129,20 +142,34 @@ function CSVFormat(f, d) {
 		});
 
 		// Unione delle colonne
+		button.innerText = "Unione delle colonne";
 		data.forEach((e, i) => {
 			data[i] = e.join();
 		});
 
 		// Unione delle righe
+		button.innerText = "Unione delle righe";
 		data = data.join("\n");
 
 		// Creazione file e download
+		button.innerText = "Creazione del file";
 		let file = new File([new Blob([data], { type: 'text/plain' })], "LibreViewFormatted.csv", { type: "text/plain" });
 		let link = document.createElement("a");
 		link.download = file.name;
 		link.href = URL.createObjectURL(file);
+		button.innerText = "Download";
 		link.click();
 
 		console.log(file);
+
+		button.innerText = "Fatto!";
+		sleep(3000).then(() => {
+			button.innerText = "Scarica CSV Formattato";
+			button.disabled = false;
+		});
 	});
+}
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
 }
